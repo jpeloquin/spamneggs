19b65b4ffa58cf6169923afe919620849ce6e132
John Peloquin <john.peloquin@gmail.com>
2019-11-11 16:37:32 -0500

    Add functions to plot time series vars singly or together

--- spamneggs.py
+++ spamneggs.py
@@ -1,8 +1,14 @@
 import json
 import os
 import matplotlib.pyplot as plt
+from matplotlib import ticker
+from mpl_toolkits.axes_grid1 import host_subplot
+from mpl_toolkits.axisartist.parasite_axes import HostAxes, ParasiteAxes
 import numpy as np
 from warnings import warn
+# Third-party packages
+import pandas as pd
+# Same-package modules
 from .variables import *
 
 
@@ -74,3 +80,69 @@ class NDArrayJSONEncoder(json.JSONEncoder):
 def write_record_to_json(record, f):
     json.dump(record, f, indent=2, ensure_ascii=False,
               cls=NDArrayJSONEncoder)
+
+
+def plot_timeseries_var(timeseries, varname, dir_out):
+    """Plot time series variable and write the plot to disk.
+
+    This function is meant for automated sensitivity analysis.  Plots
+    will be written to disk using the standard spamneggs naming
+    conventions.
+
+    TODO: Provide a companion function that just returns the plot
+    handle, allowing customization.
+
+    """
+    step = timeseries["Step"]
+    time = timeseries["Time"]
+    values = timeseries[varname]
+    # Plot the lone variable in the table on a single axes
+    fig = plt.figure()
+    host = HostAxes(fig, [0.125, 0.2, 0.825, 0.7])
+    par1 = ParasiteAxes(host, sharey=host)
+    p_time = host.plot(time, values, marker=".")
+    p_step = par1.plot(step, values, visible=False)
+    fig.add_axes(host)
+    host.parasites.append(par1)
+    host.set_xlim(0, max(time))
+    par1.set_xlim(0, max(step))
+    new_axisline = par1.get_grid_helper().new_fixed_axis
+    par1.axis["bottom2"] = new_axisline(loc="bottom", axes=par1, offset=(0, -35))
+    par1.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+    host.set_xlabel("Time")
+    host.set_ylabel(varname)
+    par1.set_xlabel("Step")
+    host.set_title(varname)
+    fig.savefig(os.path.join(dir_out, f"timeseries_var={varname}.svg"))
+
+
+def plot_timeseries_vars(timeseries, dir_out):
+    """Plot time series variables and write the plots to disk.
+
+    This function is meant for automated sensitivity analysis.  Plots
+    will be written to disk using the standard spamneggs naming
+    conventions.
+
+    TODO: Provide a companion function that just returns the plot
+    handle, allowing customization.
+
+    """
+    timeseries = timeseries.set_index("Step")
+    nm_xaxis = "Time"
+    nms_yaxis = [nm for nm in timeseries.columns if nm != nm_xaxis]
+    # Produce one large plot with all variables
+    axarr = timeseries.plot(marker=".", subplots=True, x=nm_xaxis,
+                            legend=False)
+    for nm, ax in zip(nms_yaxis, axarr):
+        ax.set_ylabel(nm)
+    # Possible future improvement: Add parasite axis for time step ID:
+    # https://stackoverflow.com/questions/50521914/
+    fig = axarr[0].figure
+    axarr[-1].set_xlabel(nm_xaxis)
+    fig.tight_layout()
+    fig.savefig(os.path.join(dir_out, "timeseries_vars.svg"))
+    plt.close(fig)
+    # Produce one small plot for each variable
+    timeseries = timeseries.reset_index()
+    for nm in nms_yaxis:
+        plot_timeseries_var(timeseries, nm, dir_out)
