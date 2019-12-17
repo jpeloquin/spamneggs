53244ba1939a343965c387169a1443a41dadf27e
John Peloquin <john.peloquin@gmail.com>
2019-12-17 11:35:29 -0500

    Remove sensitivity_loc_ind_curve
    Remove the `sensitivity_loc_ind_curve` function because its design no
    longer fits with spamneggs.  `sensitivity_loc_ind_curve` takes as input
    a function of model parameters → time series output variables and uses
    it to generate its own dataset.  This mixes the case generation, case
    solving, and dataset analysis phases, which are separate in the new
    design of spamneggs.  Therefore `sensitivity_loc_ind_curve` was removed.

--- spamneggs.py
+++ spamneggs.py
@@ -296,62 +296,6 @@ def make_sensitivity_figures(analysis_file):
                                    f"{varname}_vs_{pname}.svg")))
 
 
-def sensitivity_loc_ind_curve(solve, cen, incr, dir_out,
-                              names, units,
-                              output_names, output_units):
-    """Local sensitivity for curve output with independently varied params.
-
-    solve := a function that accepts an N-tuple of model parameters and
-    returns the corresponding model outputs as a tuple.  The first value
-    of the output is considered the independent variable; the others are
-    the dependent variables.
-
-    cen := N-tuple of numbers.  Model parameters.  These are the central values
-    about which the local sensitivity analysis is done.
-
-    incr := N-tuple of numbers.  Increments specifying how much to change each
-    model parameter in the sensitivity analysis.
-
-    dir_out := path to directory into which to write the sensitivity
-    analysis plots.
-
-    names := N-tuple of strings naming the parameters.  These will be
-    used to label the plots.
-
-    units := N-tuple of strings specifying units for the parameters.  These will be
-    used to label the plots.
-
-    """
-    linestyles = "solid", "dashed", "dotted"
-    for i, (c, Δ, name, unit) in enumerate(zip(cen, incr, names, units)):
-        fig, ax = plt.subplots()
-        ax.set_title(f"Local model sensitivity to independent variation of {name}")
-        ax.set_xlabel(f"{output_names[0]} [{output_units[0]}]")
-        ax.set_ylabel(f"{output_names[1]} [{output_units[1]}]")
-        # Plot central value
-        p = [x for x in cen]
-        output = solve(p)
-        ax.plot(output[0], output[1], linestyle=linestyles[0],
-                color="black",
-                label=f"{name} = {c}")
-        # Plot ± increments
-        for j in (1, 2):
-            # High
-            p[i] = c + j*Δ
-            output = solve(p)
-            ax.plot(output[0], output[1], linestyle=linestyles[j],
-                    color="black",
-                    label=f"{name} = {c} ± {j*Δ}")
-            # Low
-            p[i] = c - j*Δ
-            output = solve(p)
-            ax.plot(output[0], output[1], linestyle=linestyles[j],
-                    color="black")
-        ax.legend()
-        fig.tight_layout()
-        fig.savefig(os.path.join(dir_out, f"sensitivity_local_ind_curve_-_{name}.svg"))
-
-
 class NDArrayJSONEncoder(json.JSONEncoder):
 
     def default(self, o):
