b01450caf754f3e0bfbdbb517677192738af15d0
John Peloquin <john.peloquin@gmail.com>
2020-08-26 15:54:46 -0400

    Make sensitivity plots work with 2-level analysis

--- spamneggs.py
+++ spamneggs.py
@@ -633,7 +633,7 @@ def make_sensitivity_tsvar_figures(
         m_cen = np.logical_and(m_cen, v == cen[p])
     ind = {}
     for pname, pvalues in param_values.items():
-        cnorm = mpl.colors.Norm(vmin=min(pvalues), vmax=max(pvalues))
+        cnorm = mpl.colors.Normalize(vmin=min(pvalues), vmax=max(pvalues))
         m_ind = np.ones(len(cases), dtype="bool")
         for p_oth in set(param_values.keys()) - set([pname]):
             m_ind = np.logical_and(m_ind, param_values[p_oth] == cen[p_oth])
