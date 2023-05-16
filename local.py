fb1e26c23042817ea0d8f99e9087893e15d0b4e3
John Peloquin <john.peloquin@gmail.com>
2023-05-15 20:54:10 -0400

    Add least-squares cost function factory

--- local.py
+++ local.py
@@ -7,6 +7,44 @@ from .plot import (
 )
 
 
+class LsqCostFunctionFactory:
+    def __init__(self, f, callback=None):
+        """Return instance to create cost functions at a parameter point
+
+        :param f: Model function from which to derive a cost function.  Must accept a
+        vector of parameter values of length n.
+
+        :param callback: Function with call signature (parameter values, cost). Every
+        time the returned function `ψs` is called, it will call the callback with the
+        parameter values it was given and the cost it calculated. The callback is
+        usually used for logging."""
+        self.f = f
+        self.callback = callback
+
+    def make_ψ(self, x0):
+        """Return cost function referenced to true parameter values x0
+
+        :param x0: Vector of "true" parameter values on length n.  The returned cost
+        function will calculate cost relative to `f(x0)`.
+
+        :returns: Sum-squares cost function, which accepts a vector of parameters
+        with length matching x0.
+        """
+
+        f0 = self.f(x0)
+
+        def ψ(x):
+            # run_case verifies that must points are used and (via run_febio_checked)
+            # that FEBio observed them, so we don't need to check for mismatched times
+            f_value = self.f(x)
+            cost = np.sum((f_value - f0) ** 2)
+            if self.callback is not None:
+                self.callback(x, cost)
+            return cost
+
+        return ψ
+
+
 def plot_sample(analysis, id_label, Hs, H):
     # Plot the scaled Hessian matrix
     dir_Hs = analysis.directory / "samples_plots_scaled_Hessian"
