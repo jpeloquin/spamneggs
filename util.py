7a6019024a80c89525b0bb717f38a7490978a571
John Peloquin <john.peloquin@gmail.com>
2024-10-16 15:49:11 -0400

    Add db for scipy optimizer iterations

--- util.py
+++ util.py
@@ -10,8 +10,12 @@ import inspect
 import struct
 import threading
 import weakref
+from functools import wraps
+from typing import Optional, List, Callable
 
+import numcodecs
 import numpy as np
+import scipy
 import zarr
 
 
@@ -153,6 +157,137 @@ class EvaluationDB:
                 self.root["eval_values"][x_hash][k] = y[k]
         else:
             self.root["eval_values"][x_hash] = y
+
+
+class ScipyOptimizationDB:
+    """Log SciPy optimizer iterations and final result
+
+    To record function evaluations, use EvaluationDB.
+
+    """
+
+    def __init__(self, pth, mode="r"):
+        self.store = zarr.DirectoryStore(pth)
+        self.root = zarr.open(self.store, mode=mode)
+        if not self.root.read_only:
+            self.root.create_group("problem")
+            self.root.create_group("setup")
+            self.root.create_group("iterations")
+            self.root.create_group("final")
+
+    def make_scipy_callback(self):
+        """Return a callback function for scipy.optimize.minimize"""
+        counter = Counter()
+
+        def scipy_callback(intermediate_result: scipy.optimize.OptimizeResult):
+            # The name of the argument must be 'intermediate_result' for scipy to
+            # provide an OptimizeResult.
+            counter.increment()
+            return self.write_iteration(counter.count, intermediate_result)
+
+        return scipy_callback
+
+    def _write_result_to_group(self, result: scipy.optimize.OptimizeResult, group):
+        group["iteration"] = result["nit"]
+        group["fun value"] = result["fun"]
+        group["x"] = result["x"]
+        if self.algorithm == "trust-constr":
+            group["gradient"] = result["grad"]
+            group["constraint values"] = result["constr"]
+            group["constraint jacobian"] = result["jac"][0].toarray()
+            group["barrier parameter"] = result["barrier_parameter"]
+            group["barrier tolerance"] = result["barrier_tolerance"]
+            group["lagrangian gradient"] = result["lagrangian_grad"]
+            group["optimality"] = result["optimality"]
+            group["execution time"] = result["execution_time"]
+
+    def write_setup(
+        self, parameters, method, y_obs, x_init, x_true=None, cost_true=None
+    ):
+        self.root["problem"].create_dataset(
+            "parameters",
+            dtype=object,
+            object_codec=numcodecs.VLenUTF8(),
+            shape=len(parameters),
+        )
+        self.root["problem/parameters"] = parameters
+        self.root["setup"].create_dataset("method", dtype=str, shape=0)
+        self.root["setup/method"] = method
+        # ^ have to access value as self.root["setup"]["method"][""]; very odd design
+        self.root["setup/f_obs"] = y_obs
+        self.root["setup/x_init"] = x_init
+        if x_true is not None:
+            self.root["setup/x_true"] = x_true
+        if cost_true is not None:
+            self.root["setup/cost_true"] = cost_true
+
+    @property
+    def algorithm(self):
+        return self.root["setup"]["method"][""]
+
+    def get_iter_ids(self):
+        """Return sorted iteration IDs"""
+        return sorted([int(i) for i in self.root["iterations"].keys()])
+
+    def iteration_data(self, keys: Optional[List[str]] = None):
+        """Return iteration data as arrays
+
+        :param keys: List of keys for which to retrieve data.  By default, data for
+        all available keys will be returned.
+
+        """
+        if keys is None:
+            keys = self.root["iterations/0"].keys()
+        iter_ids = self.get_iter_ids()
+        data = {
+            k: np.stack([np.array(self.root["iterations"][i][k]) for i in iter_ids])
+            for k in keys
+        }
+        # Some of Scipy's algorithms don't report iteration count
+        if "iteration" in keys:
+            data["iteration"] = np.stack(
+                [np.array(self.root["iterations"][i]["iteration"]) for i in iter_ids]
+            )
+        else:
+            data["iteration"] = iter_ids
+        return data
+
+    @property
+    def parameters(self):
+        return np.array(self.root["problem/parameters"])
+
+    # I think appending to an array would be slow, but not sure
+    # https://github.com/zarr-developers/zarr-python/issues/583
+    # https://zarr.readthedocs.io/en/stable/tutorial.html
+    def write_iteration(self, id_, intermediate_result: scipy.optimize.OptimizeResult):
+        g = self.root.create_group(f"iterations/{id_}")
+        if self.algorithm == "trust-constr":
+            self._write_result_to_group(intermediate_result, g)
+        else:
+            # For Nelder-Mead, only "fun" and "x" are guaranteed to be present
+            g["x"] = intermediate_result["x"]
+            g["fun value"] = intermediate_result["fun"]
+
+    def write_final_result(self, result: scipy.optimize.OptimizeResult):
+        # Write data not part of an intermediate result
+        self.root["final/termination status"] = result["status"]
+        self.root["final"].create_dataset("termination message", dtype=str, shape=0)
+        self.root["final/termination message"] = result["message"]
+        # Write data that is part of an intermediate result (includes commit)
+        self._write_result_to_group(result, self.root["final"])
+
+    def close(self):
+        self.store.close()
+
+
+def count_calls(fn: Callable) -> Callable:
+    @wraps(fn)
+    def g(*args, **kwargs):
+        g.call_counter.increment()
+        return fn(*args, **kwargs)
+
+    g.call_counter = Counter()
+    return g
 
 
 def parameter_to_str(parameter):
